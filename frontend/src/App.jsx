f693e7b52efbdeaa900373aaccb546adf99e9714
mehak2807 <mehaktaneja2807@gmail.com>
2025-07-29 22:47:19 +0530

    added contact us form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/About";
+import Contact from "./Components/contact";
 import AdStrip from "./Components/Ad";
 import { FeaturesSection } from "./Components/Features";
 import Footer from "./Components/footer";
 import ScrollRevealWrapper from "./Components/ui/ScrollRevealWrapper";
+import { Toaster } from "sonner";
 
 function App() {
   return (
+
     <div className="min-h-screen w-full bg-gradient-to-b from-[#E4ECF1] to-[#D2DEE7] scroll-smooth overflow-hidden">
       {/* Navbar */}
       <Navbar />
@@ -35,6 +38,12 @@ function App() {
         <div id="about">
           <About />
         </div>
+        <ScrollRevealWrapper delay={0.2}>
+        <div id="contact">
+        <Toaster position="top-center" />
+          <Contact/>
+        </div>
+        </ScrollRevealWrapper>
 
         <Footer />
       </main>
