386e56d4cf44a0069102a75ccd87cc180adcdf05
Vaishnavi Sampat Shinde <vaishnavishinde1917@gmail.com>
2025-09-08 21:09:45 +0530

    Remove unnecessary test files and clean dependencies

--- backend/testEnv.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("Google Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
-console.log("Mongo URI:", process.env.MONGO_URI);
