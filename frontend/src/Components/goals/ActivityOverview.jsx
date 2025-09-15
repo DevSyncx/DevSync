d6b465f91aa1fce5ee39ea2c3fc5542863873a56
ankitasure23 <bikughoshbst@gmail.com>
2025-09-15 08:50:40 +0530

    Made the challenges.jsx

--- frontend/src/Components/goals/ActivityOverview.jsx
+++ frontend/src/Components/goals/ActivityOverview.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 
 const ActivityOverview = ({ activityData }) => {
   return (
-    <div className="bg-white shadow rounded p-4 mb-6">
-      <h3 className="font-semibold text-gray-700 mb-2">Activity Overview</h3>
-      <div className="flex space-x-1">
+    <div className='bg-[#1a2b47] shadow rounded p-4 mb-6'>
+      <h3 className='font-bold text-shadow-white mb-2'>Activity Overview</h3>
+      <div className='flex space-x-1'>
         {activityData.map((day, index) => (
-          <div
-            key={index}
-            className={`w-4 h-4 rounded-full ${day ? 'bg-green-500' : 'bg-gray-300'}`}
-            title={`Day ${index + 1}`}
-          ></div>
+          <div key={index} className={`w-4 h-4 rounded-full ${day ? 'bg-indigo-600' : 'bg-gray-400'}`} title={`Day ${index + 1}`}>
+          </div>
         ))}
       </div>
     </div>
